19284ad00c41e5c9888b77167b5a2fa9009f107f
ragnarlodbrok1992 <maraus92@gmail.com>
2025-11-10 18:38:56 +0100

    WIP

---
+++ 010-tasks/012-fizzbuzz-prime-factoring/fizzbuzz.go
@@ -0,0 +1,10 @@
+// Create a fizzbuzz application with a little twist.
+// 1. User provides upper limit of values to test.
+// 2. User provides a value that we will do prime factorization on (https://mathworld.wolfram.com/PrimeFactorization.html).
+// 3. Program performs prime factorization on provided value and finds highest and lowest prime of said number (ex. prime factors of 65347 is 101 and 647)
+// 4. Program performs printing out of fizz for lowest prime factor and buzz for highest prime factor.
+// For clarity omit all the values that doesn't provide either 'fizz' or 'buzz' string. Print out also the value near the string.
+
+func main() {
+	// Put code here
+}
