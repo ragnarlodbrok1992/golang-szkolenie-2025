04ec1f087ecf480bdf68d929602984236aefe0ba
ragnarlodbrok1992 <maraus92@gmail.com>
2025-11-10 20:08:41 +0100

    WIP

---
+++ 010-tasks/015-distributed-key-value-store/distributed_key_value_store.go
@@ -0,0 +1,11 @@
+package main
+
+// Create a distributed key value store (also called key-value database). Think of it as a giant dictionary.
+// 1. System should communicate using PUT, GET and DELETE http methods.
+// 2. It should provide one container to store the values.
+// 3. All actions in the application should be logged to a stdout.
+// 4. User provides number of simultanous instances of database which are synchronized and updated asynchronously.
+
+func main() {
+	// Put the code here.
+}
---
+++ 010-tasks/015-distributed-key-value-store/go.mod
@@ -0,0 +1,3 @@
+module distributed_key_value_store
+
+go 1.25.3
