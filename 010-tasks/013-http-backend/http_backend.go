b756a008cdb75dc577e7c7a8384763eaf1fb0cf9
ragnarlodbrok1992 <maraus92@gmail.com>
2025-11-10 19:27:34 +0100

    WIP

---
+++ 010-tasks/013-http-backend/http_backend.go
@@ -0,0 +1,10 @@
+// Create a basic HTTP backend server in Go.
+// 1. The program should listen to requests on some port (ex. 8080) and accept incoming http requests.
+// 2. Application should provide routing to specific paths.
+// 3. Application should generate reponses with status codes, headers, bodies in plain text and/or JSON.
+// 4. Application should log all requests with method, path, status code (200, 404, etc).
+// 5. Should only use Go's standard library.
+
+func main() {
+	// Put the code here
+}
